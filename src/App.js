1127964c24cb74218f3dbc5c31ffd5ca223c7fce
Sanaulla Shaik <mr.shaiksanastocks@gmail.com>
2019-01-22 08:09:01 -0600

    Added Face Recognition

--- src/App.js
+++ src/App.js
@@ -30,8 +30,28 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiface = data.outputs[0].data.regions[0].region_info.bounding_box ;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    
+    return {
+      leftCol: clarifaiface.left_col * width,
+      topRow: clarifaiface.top_row * height,
+      rightCol: width - (clarifaiface.right_col * width),
+      bottomRow: height - (clarifaiface.bottom_row * height)     
+
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -41,13 +61,8 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
     app.models.predict( Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then( response => {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      err => {
-        console.log(err);
-
-      });
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
        /*   function(response) {
             // do something with response
             console.log(response.outpus[0].data.regions[0].region_info.bouding_box);
@@ -71,7 +86,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit={this.onButtonSubmit} />     
-        <FaceRecognition imageUrl={this.state.imageUrl}/> 
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/> 
       </div>
     );
   }
